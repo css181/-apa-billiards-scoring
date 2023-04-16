5ed3d830e6ed7387ffe658589b695ed517921ffe
Craig.Schwarzwald@gmail.com <Craig.Schwarzwald@gmail.com>
2023-04-15 23:04:57 -0400

    Added functionality for combo/carem and deadballs.  Clicking the up-next ball also takes dead/combod balls into account.

--- src/app/play-field/play-field.component.spec.ts
+++ src/app/play-field/play-field.component.spec.ts
@@ -83,17 +83,19 @@ describe('PlayFieldComponent', () => {
 
       expect(upNextImg.nativeElement.src).toContain("2ball.png");
     })
-    it('should reset back to 1 ball if it was on the 9', () => {
+    it('should reset back to 1 ball and reset sunkBallList if it was on the 9', () => {
       const upNextImg = fixture.debugElement.query(By.css('.up-next'));
       component.nextBall = 9;
+      component.sunkBallsList = [1,2,3,4,5,6,7,8];
       component.curBallImgPath = "assets/images/9ball.png";
 
       upNextImg.triggerEventHandler('click', null);
       fixture.detectChanges();
       
       expect(upNextImg.nativeElement.src).toContain("1ball.png");
+      expect(component.sunkBallsList.length).toBe(0);
     })
-    it('shoudl add 1 point to the current shooter when ball is 1 through 8', () => {
+    it('should add 1 point to the current shooter when ball is 1 through 8', () => {
       setupLagWinnerAndLoser();
       const upNextImg = fixture.debugElement.query(By.css('.up-next'));
       expect(upNextImg.nativeElement.src).toContain("1ball.png");
@@ -139,7 +141,7 @@ describe('PlayFieldComponent', () => {
       fixture.detectChanges();
       expect(component.curShootingPlayer.curScore).toBe(8);
     })
-    it('shoudl add 2 point to the current shooter when ball was 9', () => {
+    it('should add 2 point to the current shooter when ball was 9', () => {
       setupLagWinnerAndLoser();
       const upNextImg = fixture.debugElement.query(By.css('.up-next'));
       component.nextBall = 9;
@@ -150,6 +152,30 @@ describe('PlayFieldComponent', () => {
       upNextImg.triggerEventHandler('click', null);
       fixture.detectChanges();
       expect(component.curShootingPlayer.curScore).toBe(2);
+    })
+    it('should add the current ball to the sunkBallList', ()=> {
+      expect(component.sunkBallsList.length).toBe(0);
+
+      const upNextImg = fixture.debugElement.query(By.css('.up-next'));
+      upNextImg.triggerEventHandler('click', null); //Click 1 ball
+      fixture.detectChanges();
+      upNextImg.triggerEventHandler('click', null); //Click 2 ball
+      fixture.detectChanges();
+
+      expect(component.sunkBallsList).toContain(1);
+      expect(component.sunkBallsList).toContain(2);
+    })
+    it('should update the current next ball to the next one not sunk if the current ball is clicked combo/carem', ()=> {
+      component.nextBall = 3;
+      component.curBallImgPath = "assets/images/3ball.png";
+      component.sunkBallsList = [1,2,4,5];
+      
+      const upNextImg = fixture.debugElement.query(By.css('.up-next'));
+      upNextImg.triggerEventHandler('click', null);
+      fixture.detectChanges();
+
+      expect(component.nextBall).toBe(6);
+      expect(component.curBallImgPath).toContain('6ball.png');
     })
   })
 
@@ -199,41 +225,191 @@ describe('PlayFieldComponent', () => {
         })
       })
     })
+  })
 
-    describe('innings', () => {
-      it('should display an innings title, and number of innings which starts at 0', () => {
-        expect(fixture.debugElement.query(By.css('#inningsTitle')).nativeElement.textContent).toBe('Innings');
-        expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('0');
+  
+  describe('innings', () => {
+    it('should display an innings title, and number of innings which starts at 0', () => {
+      expect(fixture.debugElement.query(By.css('#inningsTitle')).nativeElement.textContent).toBe('Innings');
+      expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('0');
+    })
+    it('should increment the inning count when "Add Inning" button is pressed', () => {
+      clickAddInningButton();
+
+      expect(component.innings).toBe(1);
+      expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('1');
+    })
+    it('should decrement the inning count when "Decrement Inning" button is pressed', () => {
+      clickAddInningButton();
+      expect(component.innings).toBe(1);
+
+      clickDecrementInningButton();
+
+      expect(component.innings).toBe(0);
+      expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('0');
+    })
+    it('should NOT decrement the inning count below 0 when "Decrement Inning" button is pressed', () => {
+      expect(component.innings).toBe(0);
+
+      clickDecrementInningButton();
+
+      expect(component.innings).toBe(0);
+      expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('0');
+    })
+  })
+  
+  describe('dead balls', () => {
+    it('should display an dead balls title, and number of dead balls which starts at 0', () => {
+      expect(fixture.debugElement.query(By.css('#deadBallsTitle')).nativeElement.textContent).toBe('Dead Balls');
+      expect(fixture.debugElement.query(By.css('#deadBallsValue')).nativeElement.textContent).toBe('0');
+    })
+  })
+
+  describe('row of balls 1 through 9 at the bottom', () => {
+    it('should start in not-isDeadBallMode, and clicking deadBallMode button will switch to isDeadBallMode', () => {
+      expect(component.isDeadBallMode).toBeFalse();
+
+      clickDeadBallModeButton();
+
+      expect(component.isDeadBallMode).toBeTrue();
+    })
+
+    describe('when isDeadBallMode is true', ()=> {
+      beforeEach(()=> {
+        clickDeadBallModeButton();
       })
-      it('should increment the inning count when "Add Inning" button is pressed', () => {
-        clickAddInningButton();
-
-        expect(component.innings).toBe(1);
-        expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('1');
+      it('should display the balls with opacity < 1', ()=> {
+        const ball1Img = fixture.debugElement.query(By.css("#ball1")).nativeElement;
+        expect(getComputedStyle(ball1Img).opacity).toBeLessThan(1);
       })
-      it('should decrement the inning count when "Decrement Inning" button is pressed', () => {
-        clickAddInningButton();
-        expect(component.innings).toBe(1);
-
-        clickDecrementInningButton();
-
-        expect(component.innings).toBe(0);
-        expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('0');
+      it('should show the 9 ball can not die image', ()=> {
+        const ball9 = fixture.debugElement.query(By.css('#ball9'));
+        expect(ball9.nativeElement.src).toContain("9ball_dead.png");
       })
-      it('should NOT decrement the inning count below 0 when "Decrement Inning" button is pressed', () => {
-        expect(component.innings).toBe(0);
+      it('should not impact score, dead ball count, or image when the 9ball is clicked (because it can not die)', ()=> {
+        const score = component.curShootingPlayer.curScore;
+        const deadBallCount = component.deadBalls;
+        const curImg = fixture.debugElement.query(By.css('.up-next')).nativeElement.src;
 
-        clickDecrementInningButton();
+        const ball9 = fixture.debugElement.query(By.css('#ball9'));
+        ball9.triggerEventHandler('click', null);
+        fixture.detectChanges();
 
-        expect(component.innings).toBe(0);
-        expect(fixture.debugElement.query(By.css('#inningsValue')).nativeElement.textContent).toBe('0');
+        expect(component.curShootingPlayer.curScore).toBe(score);
+        expect(component.deadBalls).toBe(deadBallCount);
+        expect(fixture.debugElement.query(By.css('.up-next')).nativeElement.src).toBe(curImg);
+      })
+      it('should add a dead ball for each ball that gets clicked (other than the 9 which can not die)', ()=> {
+        expect(component.deadBalls).toBe(0);
+        const ball1 = fixture.debugElement.query(By.css("#ball1"));
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.deadBalls).toBe(1);
+      })
+      it('should add the balls number to the sunkBalls list when clicked and change that balls opacity to less than 0.5', ()=> {
+        const ball1 = fixture.debugElement.query(By.css("#ball1"));
+        expect(getComputedStyle(ball1.nativeElement).opacity).toBeGreaterThanOrEqual(0.5);
+        expect(component.sunkBallsList.length).toBe(0);
+
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(getComputedStyle(ball1.nativeElement).opacity).toBeLessThan(0.5);
+        expect(component.sunkBallsList).toContain(1);
+      })
+      it('should should not add another dead ball if the same dead ball is clicked multiple times', ()=> {
+        const ball1 = fixture.debugElement.query(By.css("#ball1"));
+        expect(component.deadBalls).toBe(0);
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(component.deadBalls).toBe(1);
+
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.deadBalls).toBe(1);
+      })
+      it('should not update the current next ball if a different ball is clicked dead', ()=> {
+        expect(component.nextBall).toBe(1);
+
+        const ball2 = fixture.debugElement.query(By.css("#ball2"));
+        ball2.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.nextBall).toBe(1);
+      })
+      it('should update the current next ball to the next one not sunk if the current ball is clicked dead', ()=> {
+        component.nextBall = 3;
+        component.sunkBallsList = [1,2,4,5];
+
+        const ball3 = fixture.debugElement.query(By.css("#ball3"));
+        ball3.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.nextBall).toBe(6);
       })
     })
-    
-    describe('dead balls', () => {
-      it('should display an dead balls title, and number of dead balls which starts at 0', () => {
-        expect(fixture.debugElement.query(By.css('#deadBallsTitle')).nativeElement.textContent).toBe('Dead Balls');
-        expect(fixture.debugElement.query(By.css('#deadBallsValue')).nativeElement.textContent).toBe('0');
+
+    describe('when isDeadBallMode is false', ()=> {
+      it('should display the balls with opacity = 1', ()=> {
+        const ball1Img = fixture.debugElement.query(By.css("#ball1")).nativeElement;
+        expect(getComputedStyle(ball1Img).opacity).toEqual('1');
+      })
+      it('should add the balls number to the sunkBalls list when clicked and change that balls opacity to less than 0.5', ()=> {
+        const ball1 = fixture.debugElement.query(By.css("#ball1"));
+        expect(getComputedStyle(ball1.nativeElement).opacity).toBeGreaterThanOrEqual(0.5);
+        expect(component.sunkBallsList.length).toBe(0);
+
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(getComputedStyle(ball1.nativeElement).opacity).toBeLessThan(0.5);
+        expect(component.sunkBallsList).toContain(1);
+      })
+      it('should should not add more points if the same carem/combo ball is clicked multiple times', ()=> {
+        const ball1 = fixture.debugElement.query(By.css("#ball1"));
+        expect(component.curShootingPlayer.curScore).toBe(0);
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(component.curShootingPlayer.curScore).toBe(1);
+
+        ball1.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.curShootingPlayer.curScore).toBe(1);
+      })
+      it('should not update the current next ball if a different ball is clicked combo/carem', ()=> {
+        expect(component.nextBall).toBe(1);
+
+        const ball2 = fixture.debugElement.query(By.css("#ball2"));
+        ball2.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.nextBall).toBe(1);
+      })
+      it('should update the current next ball to the next one not sunk if the current ball is clicked combo/carem', ()=> {
+        component.nextBall = 3;
+        component.sunkBallsList = [1,2,4,5];
+
+        const ball3 = fixture.debugElement.query(By.css("#ball3"));
+        ball3.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.nextBall).toBe(6);
+      })
+      it('should add 2 points, reset nextball to 1, and reset sunkBallList to empty when 9 is clicked combo/carem', ()=> {
+        component.nextBall = 3;
+        component.sunkBallsList = [1,2,4,5];
+        expect(component.curShootingPlayer.curScore).toBe(0);
+
+        const ball9 = fixture.debugElement.query(By.css("#ball9"));
+        ball9.triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(component.curShootingPlayer.curScore).toBe(2);
+        expect(component.nextBall).toBe(1);
+        expect(component.sunkBallsList.length).toBe(0);
       })
     })
   })
@@ -261,6 +437,11 @@ describe('PlayFieldComponent', () => {
   }
   function clickDecrementInningButton() {
     let button = fixture.debugElement.query(By.css('button#decrementInning'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+  }
+  function clickDeadBallModeButton() {
+    let button = fixture.debugElement.query(By.css('button#deadBallMode'));
     button.triggerEventHandler('click', null);
     fixture.detectChanges();
   }
--- src/app/play-field/play-field.component.ts
+++ src/app/play-field/play-field.component.ts
@@ -28,6 +28,8 @@ export class PlayFieldComponent implements OnInit{
   public curShootingPlayer: ICurrentPlayer = {} as ICurrentPlayer;
   public innings: number = 0;
   public deadBalls: number = 0;
+  public isDeadBallMode: boolean = false;
+  public sunkBallsList: number[] = [];
 
   constructor(public sharedData: SharedDataService, private route: ActivatedRoute) { }
 
@@ -47,17 +49,68 @@ export class PlayFieldComponent implements OnInit{
     this.curShootingPlayer = this.lagWinningPlayer;
   }
 
-  updateNextBall(newNext: number) {
+  updateNextBall(curBall: number) {
     let pointValue:number = 1;
-    if(newNext > 9) {
-      newNext = 1;
-      pointValue=2;
+    this.sunkBallsList.push(this.nextBall);
+    if(curBall === 9) {
+        pointValue=2;
+        this.resetBallsToNewGame();
     }
-    console.log(this.curShootingPlayer.name + ' hit ' + this.nextBall);
-    this.nextBall = newNext;
-    this.curBallImgPath = "assets/images/" + newNext + "ball.png";
+    console.log(this.curShootingPlayer.name + ' hit next ball: ' + curBall);
+    this.assignNewNextBall();
+    this.curBallImgPath = "assets/images/" + this.nextBall + "ball.png";
     this.curShootingPlayer.curScore+=pointValue;
     console.log(this.curShootingPlayer.name + ' new score = ' + this.curShootingPlayer.curScore);
+  }
+  
+  addDeadBall(ballNum: number) {
+    if(this.sunkBallsList.indexOf(ballNum)==-1){
+      this.deadBalls++;
+      this.sunkBallsList.push(ballNum);
+      if(this.nextBall===ballNum) {
+        this.assignNewNextBall();
+      }
+    }
+  }
+
+  caremComboBall(ballNum: number) {
+    if(this.sunkBallsList.indexOf(ballNum)==-1){
+      this.sunkBallsList.push(ballNum);
+      let pointValue:number = 1;
+      if(ballNum == 9) {
+          this.resetBallsToNewGame();
+          pointValue=2;
+      }
+      if(this.nextBall===ballNum) {
+        this.assignNewNextBall();
+      }
+      this.curShootingPlayer.curScore+=pointValue;
+      console.log(this.curShootingPlayer.name + ' combo/caremed ' + ballNum);
+      console.log(this.curShootingPlayer.name + ' new score = ' + this.curShootingPlayer.curScore);
+    }
+  }
+
+  resetBallsToNewGame(): void {
+    this.nextBall = 1;
+    this.curBallImgPath = "assets/images/1ball.png";
+    this.sunkBallsList = [];
+  }
+
+  assignNewNextBall(): void {
+    let newNext = -1;
+    for(let x=1; x<10; x++) {
+      if(this.sunkBallsList.indexOf(x)===-1) {
+        newNext = x;
+        break;
+      }
+    }
+    //Extra caution.
+    if(newNext===-1) {
+      console.error('We somehow could not figure out the next ball on a carem/combo or deadball... setting to 1');
+      newNext = 1;
+    }
+    this.nextBall=newNext;
+    this.curBallImgPath = "assets/images/" + this.nextBall + "ball.png";
   }
 
   onEndTurn(): void {
@@ -69,6 +122,10 @@ export class PlayFieldComponent implements OnInit{
       this.innings++;
       console.log('adding inning');
     }
+  }
+
+  flipDeadBallMode(): void {
+    this.isDeadBallMode = !this.isDeadBallMode;
   }
 
   onAddInning(): void {
